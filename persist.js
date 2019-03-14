2ac907e5fc815d15e811a8bced4f646329ec7ef4
Agus Prasetiyo <agus.prasetiyo@traveloka.com>
2019-03-14 16:47:43 +0700

    leftover

--- persist.js
+++ persist.js
@@ -9,7 +9,6 @@ function persistAWSCredentials(credentialData) {
     "aws_secret_access_key": credentialData['SecretAccessKey'],
     "aws_session_token": credentialData['SessionToken']
   };
-  // fs.writeFileSync(shared_config_file_loc, ini.stringify(aws_config, { section: 'saml' }))
   util.promisify(fs.writeFile)(shared_config_file_loc, ini.stringify(aws_config, { section: 'saml' }));
   return credentialData;
 }
